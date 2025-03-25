8a97946aad80a6a5d2d4a69baa509cabf5563a09
JL4817 <89381622+JL4817@users.noreply.github.com>
2025-03-25 08:35:31 +0100

    commit1.1

--- ID_279B.java
+++ ID_279B.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class ID_279B {
@@ -5,6 +6,14 @@ public class ID_279B {
         Scanner input = new Scanner(System.in);
         int n = input.nextInt();
         int t = input.nextInt();
+        int count = 0;
         input.nextLine();
+        int[] arr = Arrays.stream(input.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        Arrays.sort(arr);
+
+        for (int i = 0; i < arr.length; i++) {
+
+        }
+
     }
 }
